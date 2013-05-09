64cf0ee3e937ebdea1cd05087f8335237b13275a
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2013-05-10 01:01:00 +0400

    changes to plotting

--- plots_c.py
+++ plots_c.py
@@ -1,17 +1,20 @@
 import random
 import subprocess
+import time
 
 from matplotlib import pylab
 
 
 if __name__ == '__main__':
     random.seed(42)
-    n = 50
+    #n = 50
 
     for _ in range(10):
+        n = random.randrange(50, 101)
         xs = []
         ys = []
-        print _
+        print _, n,
+        start = time.clock()
         p = subprocess.Popen(
             ['FragileMirrors/Release/FragileMirrors.exe'],
             stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -24,6 +27,7 @@ if __name__ == '__main__':
         out, err = p.communicate(input)
         ret_code = p.wait()
         assert ret_code == 0
+        print time.clock() - start
 
         for line in err.split('\n'):
             line = line.strip()
@@ -37,8 +41,8 @@ if __name__ == '__main__':
 
                 xs.append(x)
                 ys.append(y)
-                #print x, y
-    #print out
-        pylab.plot(xs, ys, '.')
+
+        style = {5: '.r', 6: '.y', 7: '.g', 8: '.b', 9: '.k', 10: '.k'}[n//10]
+        pylab.plot(xs, ys, style)
     pylab.plot([0, 1], [0, 1], '-')
     pylab.show()
